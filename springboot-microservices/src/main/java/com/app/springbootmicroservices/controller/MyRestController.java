ea88ed3ea7e91f1ad64205e7f5fc57f2f8c3f7dc
khasakinil <khasakinil@gmail.com>
2024-04-19 17:12:42 +0530

    rest controller added

---
+++ springboot-microservices/src/main/java/com/app/springbootmicroservices/controller/MyRestController.java
@@ -0,0 +1,15 @@
+package com.app.springbootmicroservices.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/rest")
+public class MyRestController {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Hi Everyone, Good morning";
+	}
+}
