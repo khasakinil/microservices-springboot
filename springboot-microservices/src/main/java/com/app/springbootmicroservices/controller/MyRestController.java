35fda9066e2fc12624633a1a3b1c3d3c23239d71
khasakinil <khasakinil@gmail.com>
2024-05-31 22:28:04 +0530

    custome property read in application with @Value

--- springboot-microservices/src/main/java/com/app/springbootmicroservices/controller/MyRestController.java
+++ springboot-microservices/src/main/java/com/app/springbootmicroservices/controller/MyRestController.java
@@ -1,5 +1,6 @@
 package com.app.springbootmicroservices.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +9,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/rest")
 public class MyRestController {
 
+	@Value("${team.name}")
+	private String teamName;
+
 	@GetMapping("/message")
 	public String getMessage() {
 		return "Hi Everyone, Good morning";
 	}
-	
+
 	@GetMapping("/fortune")
 	public String getDailyFortune() {
 		return "Today is lucky day..!!";
+	}
+
+	@GetMapping("/teamname")
+	public String getTeamName() {
+		return "Team : " + teamName;
 	}
 }
