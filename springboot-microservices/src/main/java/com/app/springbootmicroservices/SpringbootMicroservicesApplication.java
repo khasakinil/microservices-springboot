ad9f3d0ecf0585447a806e29611e80cc6894d328
khasakinil <khasakinil@gmail.com>
2024-06-04 16:23:38 +0530

    mysql db configurations

--- springboot-microservices/src/main/java/com/app/springbootmicroservices/SpringbootMicroservicesApplication.java
+++ springboot-microservices/src/main/java/com/app/springbootmicroservices/SpringbootMicroservicesApplication.java
@@ -1,7 +1,9 @@
 package com.app.springbootmicroservices;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootMicroservicesApplication {
@@ -10,4 +12,10 @@ public class SpringbootMicroservicesApplication {
 		SpringApplication.run(SpringbootMicroservicesApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Executed after the Spring Beans have been loaded..");
+		};
+	}
 }
